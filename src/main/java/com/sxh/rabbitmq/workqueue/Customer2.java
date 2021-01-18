c89cead986da6f5f7abfe02cc6060760c277b88c
sxh <xuhang_shang@163.com>
2021-01-18 23:08:01 +0800

    【任务/工作 消息模型】一对多的任务模型，一个Provider 产生的大量消息，多个Customer 轮询消费消息；缺点：每个Customer 处理的消息数量一样，当个别Customer 处理耗时过大时，会影响任务效率；

---
+++ src/main/java/com/sxh/rabbitmq/workqueue/Customer2.java
@@ -0,0 +1,45 @@
+package com.sxh.rabbitmq.workqueue;
+
+import com.rabbitmq.client.*;
+import com.sxh.rabbitmq.util.ConnectionUtil;
+
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author SXH
+ * @description 任务模型  一对多 多 <---> 消费者
+ * @date 2021/1/18 22:52
+ */
+public class Customer2 {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 1.使用工具类(Singleton)获取连接对象
+        Connection connection = ConnectionUtil.creatConnection();
+
+        // 2.获取连接中的通道
+        Channel channel = connection.createChannel();
+
+        // 3.通道绑定对应的消息队列
+        // 参数1：队列名称，如果队列不存在，自动创建
+        // 参数2：durable  用来定义队列特性是否要持久化  true-持久化  false-不持久化
+        // 参数3：exclusive 是否独占队列 true-独占队列 false-不独占
+        // 参数4：autoDelete 是否消费完后自动删除队列 true-自动删除  false-不自动删除
+        // 参数5：arguments 额外附加参数
+        channel.queueDeclare("work", true, false, true, null);
+
+        // 4.消费消息
+        channel.basicConsume("work", true, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) {
+//                super.handleDelivery(consumerTag, envelope, properties, body);
+                System.out.println(LocalTime.now() + "消费者2----------->" + new String(body));
+            }
+        });
+
+        // 由于是消费者，所以需要一直监控队列的任务，不用关闭channel 和connection
+//        channel.close();
+//        connection.close();
+    }
+}
---
+++ src/main/java/com/sxh/rabbitmq/workqueue/Provider.java
@@ -0,0 +1,46 @@
+package com.sxh.rabbitmq.workqueue;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.MessageProperties;
+import com.sxh.rabbitmq.util.ConnectionUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author SXH
+ * @description 任务模型  一对多   一 <---> 生产者
+ * @date 2021/1/18 22:49
+ */
+public class Provider {
+    // 生产消息
+    @Test
+    public void testSendMessage() throws IOException, TimeoutException {
+        // 1.使用工具类(Singleton)获取连接对象
+        Connection connection = ConnectionUtil.creatConnection();
+
+        // 2.获取连接中的通道
+        Channel channel = connection.createChannel();
+
+        // 3.通道绑定对应的消息队列
+        // 参数1：队列名称，如果队列不存在，自动创建
+        // 参数2：durable  用来定义队列特性是否要持久化  true-持久化  false-不持久化
+        // 参数3：exclusive 是否独占队列 true-独占队列 false-不独占
+        // 参数4：autoDelete 是否消费完后自动删除队列 true-自动删除  false-不自动删除
+        // 参数5：arguments 额外附加参数
+        channel.queueDeclare("work", true, false, true, null);
+
+        // 4.发布消息
+        for (int i = 0; i < 20; i++) {
+            // 参数1：exchange 交换机名称
+            // 参数2：routingKey 队列名称
+            // 参数3：传递消息额外配置
+            // 参数4：消息的具体内容
+            channel.basicPublish("", "work", null, (i + "work rabbitmq").getBytes());
+        }
+        // 5.关闭连接
+        ConnectionUtil.closeConnectionAndChanel(channel, connection);
+    }
+}
