3773ec0f186b46d54b387a72e0f403589aa2c2b8
sxh <xuhang_shang@163.com>
2021-01-19 23:08:20 +0800

    【Routing订阅模式-Direct】Routing 订阅模式，direct 直连，消息发送给指定routing 绑定的队列，消息不用发送给所有与exchange 交换机绑定的队列。

---
+++ src/main/java/com/sxh/rabbitmq/direct/Customer2.java
@@ -0,0 +1,37 @@
+package com.sxh.rabbitmq.direct;
+
+import com.rabbitmq.client.*;
+import com.sxh.rabbitmq.util.ConnectionUtil;
+
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author SXH
+ * @description Routing 订阅模式， direct 直连  消费者
+ * @date 2021/1/19 22:56
+ */
+public class Customer2 {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 1.获得rabbitmq 链接
+        Connection connection = ConnectionUtil.creatConnection();
+        // 2.获得channel
+        Channel channel = connection.createChannel();
+        // 3.定义exchange 交换机
+        channel.exchangeDeclare("directExchange", "direct");
+        // 4.获得随机队列，并与交换机绑定
+        String queueName = channel.queueDeclare().getQueue();
+        // 参数3：路由名   绑定多个routing
+        channel.queueBind(queueName, "directExchange", "error");
+        channel.queueBind(queueName, "directExchange", "debug");
+        channel.queueBind(queueName, "directExchange", "warning");
+        // 5.监控交换机绑定的队列数据，并消费
+        channel.basicConsume(queueName, true, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println(LocalTime.now() + "direct消费者2----------->" + new String(body));
+            }
+        });
+    }
+}
---
+++ src/main/java/com/sxh/rabbitmq/direct/Provider.java
@@ -0,0 +1,36 @@
+package com.sxh.rabbitmq.direct;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.sxh.rabbitmq.util.ConnectionUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author SXH
+ * @description Routing 订阅模式， direct 直连  生产者
+ *              消息发送给指定routing 绑定的队列，消息不用发送给所有与exchange 交换机绑定的队列。
+ * @date 2021/1/19 22:46
+ */
+public class Provider {
+    @Test
+    public void provider() throws IOException, TimeoutException {
+        // 1.获得rabbitMQ 的链接
+        Connection connection = ConnectionUtil.creatConnection();
+        // 2.创建通道
+        Channel channel = connection.createChannel();
+        // 3.声明交换机
+        // 参数1：交换机名  参数2：交换机类型
+
+        channel.exchangeDeclare("directExchange", "direct");
+        // 4.向交换机发送消息
+        String routingKey = "error";
+        channel.basicPublish("directExchange", routingKey, null,
+                ("发送消息到direct routingKey：[" + routingKey + "]").getBytes());
+
+        // 5.关闭资源
+        ConnectionUtil.closeConnectionAndChanel(channel, connection);
+    }
+}
